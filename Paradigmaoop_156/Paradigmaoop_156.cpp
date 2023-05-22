491dbef3425b7b91359631187d5cd2e84905ff58
nicodwirahman <nico.dwi.ft22@mail.umy.ac.id>
2023-05-22 15:28:34 +0700

    2

--- Paradigmaoop_156/Paradigmaoop_156.cpp
+++ Paradigmaoop_156/Paradigmaoop_156.cpp
@@ -8,5 +8,14 @@ private:
 	string nama;
 	float nilai;
 
+public:
+	mahasiswa(int pId, string pNama, float PNilai) :id(pId), nama(pNama), nilai(PNilai) {
 
+	}
+
+	~mahasiswa() {
+		cout << "id =" << id << endl;
+		cout << "Nama = " << nama << endl;
+		cout << "Nilai = " << nilai << endl;
+	}
 };
