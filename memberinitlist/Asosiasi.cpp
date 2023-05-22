6bf5163039c670fe3ef3b1cb28d06e7a7c0e12d0
nicodwirahman <nico.dwi.ft22@mail.umy.ac.id>
2023-05-22 18:48:51 +0700

    pasien

--- memberinitlist/Asosiasi.cpp
+++ memberinitlist/Asosiasi.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
+class dokter;
+class pasien {
+public:
+	string nama;
+	vector<dokter*> daftar_dokter;
+	pasien(string pNama) :nama(pNama) {
+		cout << " paasien \"" << nama << "\"ada\n";
+	}
+	
+};
