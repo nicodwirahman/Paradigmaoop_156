4a1f15c35c421278fc84a657ff75fa5b1c17bbdf
nicodwirahman <nico.dwi.ft22@mail.umy.ac.id>
2023-05-22 18:27:18 +0700

    jantung

--- mainJM/jantung.h
+++ mainJM/jantung.h
@@ -1,7 +1,17 @@
+#include <iostream>
 #ifndef JANTUNG_H
 #define JANTUNG_H
 
 using namespace std;
 
-class jantung
+class jantung {
+public:
+	jantung() {
+		cout << "jantung didupkan\n";
+	}
+	~jantung() {
+		cout << "jantung dimatikan\n";
+	}
+};
+#endif
 
